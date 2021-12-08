2bcad5373fba722d16ef77b8ce39ffad38612149
Aaron Janes <aaron.janes@gmail.com>
2021-12-07 21:41:32 -0700

    more template data

--- blog/views.py
+++ blog/views.py
@@ -46,14 +46,18 @@ all_posts = [
         """
     },
     {
-        "slug": "hike-in-the-mountains",
+        "slug": "coding",
         "image": "coding.jpeg",
         "author": "Aaron Janes",
         "date": date(2021, 7, 27),
-        "title": "Dirt Bike",
+        "title": "CODING",
         "excerpt": "There is nothing like the the views in the mountain i wasn't even prepared for what happened next",
         "content": """
-        Lorem ipsum dolor it amet consectetur adipisicing elit. Distinctio perferendis voluptatum iure numquam quia autem dolore at expedita officia harum odio minus consequatur nisi ipsam quasi aut, eum natus voluptate earum obcaecati molestiae ea ut? Numquam optio voluptates enim veniam vitae nesciunt nam explicabo ab suscipit, quos molestias, amet nisi nobis ducimus magni inventore quisquam dolorum quia? Similique ut optio, officia iste nulla natus a id numquam repudiandae voluptates autem suscipit iure molestias possimus delectus corrupti quod ad non qui. Repellat ratione impedit sapiente placeat illo voluptatum eveniet dolor ipsum iste cupiditate molestiae, dolores porro aspernatur sint quis id assumenda?
+        Lorem ipsum dolor it amet consectetur adipisicing elit. Distinctio perferendis voluptatum iure numquam quia autem dolore at expedita officia harum odio minus consequatur nisi ipsam quasi aut, eum natus voluptate earum obcaecati molestiae ea ut?
+        
+        Numquam optio voluptates enim veniam vitae nesciunt nam explicabo ab suscipit, quos molestias, amet nisi nobis ducimus magni inventore quisquam dolorum quia? Similique ut optio, officia iste nulla natus a id numquam repudiandae voluptates autem suscipit iure molestias possimus delectus corrupti quod ad non qui.
+        
+        Repellat ratione impedit sapiente placeat illo voluptatum eveniet dolor ipsum iste cupiditate molestiae, dolores porro aspernatur sint quis id assumenda?
         """
     },
 ]
@@ -72,8 +76,11 @@ def index(req):
 
 
 def posts(req):
-    return render(req, "blog/all-posts.html")
+    return render(req, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(req, slug):
-    return render(req, "blog/post-detail-page.html", )
+    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(req, "blog/post-detail-page.html", {"post":identified_post})
